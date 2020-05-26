35a213883703f5c70ca02012f926eb8fe558c04b
aemishukov <aemishukov@gmail.com>
2020-05-26 22:13:46 +0300

    add search page

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IShoeRepository shoeRepository;
+
+        public SearchController(IShoeRepository shoeRepository)
+        {
+            this.shoeRepository = shoeRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var shoes = shoeRepository.GetAllByTitle(query);
+
+            return View(shoes);
+        }
+    }
+}
--- presentation/Store.Web/Startup.cs
+++ presentation/Store.Web/Startup.cs
@@ -33,6 +33,8 @@ namespace Store.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IShoeRepository, Store.Memory.ShoeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
